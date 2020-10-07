2bdc2a9d0ecb9363f69afc79976c0a6e6dbf98c8
IagoGrah <iagobg02@gmail.com>
2020-10-06 21:10:58 -0300

    Tests/Domain folders

---
+++ Tests/Arrays2Test.cs
@@ -0,0 +1,160 @@
+using Xunit;
+using Domain;
+
+namespace Tests
+{
+    public class zUmTests
+    {
+        [Theory]
+        [InlineData(new double[]{5,4,3,2,1}, new double[]{6,7,8,9})]
+        public void should_return_switched_arrays(double[] inputA, double[] inputB)
+        {
+            var arrays2 = new Arrays2();
+
+            var result = arrays2.zUm(inputA, inputB);
+
+            Assert.Equal(inputB, result.A);
+            Assert.Equal(inputA, result.B);
+        }
+    }
+
+    public class zDoisTests
+    {
+        [Theory]
+        [InlineData(new double[]{1, -2, 3.4, 5}, false)]
+        [InlineData(new double[]{0, 0, 0, 0}, true)]
+        [InlineData(new double[]{0, 1, 2.45, 0}, true)]
+        [InlineData(new double[]{0, 0.001, 0.002}, false)]
+        public void should_return_true_if_contains_repeated_numbers(double[] input, bool expected)
+        {
+            var arrays2 = new Arrays2();
+
+            var result = arrays2.zDois(input);
+
+            Assert.Equal(expected, result);
+        }
+    }
+
+    public class zTresTests
+    {
+        [Theory]
+        [InlineData(new char[]{'f', 'm', 'f'}, new char[]{'y', 'n', 'n'}, 50)]
+        [InlineData(new char[]{'f', 'f', 'f'}, new char[]{'n', 'n', 'n'}, 0)]
+        [InlineData(new char[]{'f', 'f'}, new char[]{'y', 'y'}, 100)]
+        public void should_return_percentage_of_women_who_said_yes(char[] genders, char[] answers, double expected)
+        {
+            var arrays2 = new Arrays2();
+
+            var result = arrays2.zTres(genders, answers);
+
+            Assert.Equal(expected, result);
+        }
+    }
+
+    public class zQuatroTests
+    {
+        [Theory]
+        [InlineData(new int[]{1,2,3,4,5,6}, 9)]
+        [InlineData(new int[]{0}, 0)]
+        [InlineData(new int[]{11, 0, -43, -12}, -32)]
+        public void should_return_sum_of_odd_numbers(int[] input, int expected)
+        {
+            var arrays2 = new Arrays2();
+
+            var result = arrays2.zQuatro(input);
+
+            Assert.Equal(expected, result);
+        }
+    }
+
+    public class zCincoTests
+    {
+        [Theory]
+        [InlineData(new double[]{1, -2, -3, -0.440, 67.3}, 2)]
+        [InlineData(new double[]{0, 0, 0, -0}, 0)]
+        [InlineData(new double[]{0.00001, 987, -90192}, 2)]
+        public void should_return_how_many_positive_numbers(double[] input, int expected)
+        {
+            var arrays2 = new Arrays2();
+
+            var result = arrays2.zCinco(input);
+
+            Assert.Equal(expected, result);
+        }
+    }
+
+    public class zSeisTests
+    {
+        [Fact]
+        public void should_throw_exception_if_negative_numbers()
+        {
+            var arrays2 = new Arrays2();
+            var input = new double[]{-2, 0, 1, 2};
+
+            Assert.Throws<System.ArgumentException>(() => arrays2.zSeis(input));            
+        }
+        
+        [Theory]
+        [InlineData(new double[]{1,2,3,4,5,3}, 4)]
+        [InlineData(new double[]{0,0,1}, 2)]
+        [InlineData(new double[]{999.34,0,1}, 0)]
+        public void should_return_index_of_biggest_number(double[] input, int expected)
+        {
+            var arrays2 = new Arrays2();
+
+            var result = arrays2.zSeis(input);
+
+            Assert.Equal(expected, result);
+        }
+    }
+
+    public class zSeteTests
+    {
+        [Theory]
+        [InlineData(new double[]{0,1,2,3,4}, 5)]
+        [InlineData(new double[]{1,2,3,4}, 0)]
+        [InlineData(new double[]{0,3,2,1,4}, 3)]
+        public void should_return(double[] input, int expected)
+        {
+            var arrays2 = new Arrays2();
+
+            var result = arrays2.zSete(input);
+
+            Assert.Equal(expected, result);
+        }
+    }
+
+    public class zOitoTests
+    {
+        [Theory]
+        [InlineData(new char[]{'a', 'e', 'i', 'o', 'u'}, 5)]
+        [InlineData(new char[]{'f', 'g', 'h', 'j', 'k'}, 0)]
+        [InlineData(new char[]{'i', 'e'}, 2)]
+        [InlineData(new char[]{'t', 'a', 'e', 'o', 'p'}, 3)]
+        public void should_return_how_many_vowels(char[] input, int expected)
+        {
+            var arrays2 = new Arrays2();
+
+            var result = arrays2.zOito(input);
+
+            Assert.Equal(expected, result);
+        }
+    }
+
+    public class zNoveTests
+    {
+        [Theory]
+        [InlineData(new char[]{'o', 'u', 'l', 'p', 'a'}, "ola")]
+        [InlineData(new char[]{'f', 'g', 'h', 'j', 'k'}, "fhk")]
+        [InlineData(new char[]{'i', 'e'}, "i")]
+        [InlineData(new char[]{'t'}, "t")]
+        public void should_return_string_of_chars_on_even_index(char[] input, string expected)
+        {
+            var arrays2 = new Arrays2();
+
+            var result = arrays2.zNove(input);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ Tests/ElectionTest.cs
@@ -0,0 +1,157 @@
+using Xunit;
+using System.Collections.Generic;
+using System.Linq;
+using Domain;
+
+namespace Tests
+{
+    public class ElectionTest
+    {
+        public List<Candidate> GetMockCandidates(int amount)
+        {
+            var MockCandidates = new List<Candidate>
+            {
+                new Candidate("Jonas", "123.456.789-10"),
+                new Candidate("Ramos", "109.876.543-21"),
+                new Candidate("Bezos", "102.352.109-18"),
+                new Candidate("Jesus", "123.456.543-21")
+            };
+
+            return MockCandidates.Take(amount).ToList();
+        }
+        
+        [Fact]
+        public void should_return_false_when_wrong_password()
+        {
+            var election = new Election();
+            var candidatesInput = GetMockCandidates(1);
+
+            bool result = election.TryCreateCandidates(candidatesInput, "WrongPassword123");
+
+            Assert.False(result);
+            Assert.Empty(election.Candidates);
+        }
+        
+        [Fact]
+        public void should_return_true_when_correct_password()
+        {
+            var election = new Election();
+            var candidatesInput = GetMockCandidates(1);
+
+            bool result = election.TryCreateCandidates(candidatesInput, "Pa$$w0rd");
+
+            Assert.True(result);
+            Assert.Equal(1, election.Candidates.Count);
+        }
+
+        [Fact]
+        public void should_return_empty_if_null_input()
+        {
+            var election = new Election();
+            List<Candidate> candidatesInput = null;
+
+            bool result = election.TryCreateCandidates(candidatesInput, "WrongPassword123");
+
+            Assert.False(result);
+            Assert.Empty(election.Candidates);
+        }
+
+        [Fact]
+        public void should_return_different_ids()
+        {
+            var election = new Election();
+            var candidatesInput = GetMockCandidates(2);
+            election.TryCreateCandidates(candidatesInput, "Pa$$w0rd");
+
+            var firstId = election.GetCandidateIdsByName(candidatesInput[0].Name)[0];
+            var secondId = election.GetCandidateIdsByName(candidatesInput[1].Name)[0];
+
+            Assert.NotEqual(firstId, secondId);
+        }
+
+        [Fact]
+        public void should_return_2_ids()
+        {
+            var election = new Election();
+            var jonas1 = new Candidate("Jonas", "454.273.081.54");
+            var jonas2 = new Candidate("Jonas", "123.456.789.10");
+            var candidatesInput = new List<Candidate>{jonas1, jonas2};
+            election.TryCreateCandidates(candidatesInput, "Pa$$w0rd");
+
+            var foundIds = election.GetCandidateIdsByName("Jonas");
+
+            Assert.Equal(2, foundIds.Count);
+        }
+
+        [Fact]
+        public void should_return_jonas1_id()
+        {
+            var election = new Election();
+            var jonas1 = new Candidate("Jonas", "454.273.081.54");
+            var jonas2 = new Candidate("Jonas", "123.456.789.10");
+            var candidatesInput = new List<Candidate>{jonas1, jonas2};
+            election.TryCreateCandidates(candidatesInput, "Pa$$w0rd");
+
+            var foundId = election.GetCandidateIdByCPF(jonas1.Cpf);
+
+            Assert.Equal(jonas1.Id, foundId);
+        }
+
+        [Fact]
+        public void should_return_2_and_0_votes()
+        {
+            var election = new Election();
+            var candidatesInput = GetMockCandidates(2);
+            election.TryCreateCandidates(candidatesInput, "Pa$$w0rd");
+
+            election.Vote(candidatesInput[0].Id);
+            election.Vote(candidatesInput[0].Id);
+
+            Assert.Equal(2, candidatesInput[0].Votes);
+            Assert.Equal(0, candidatesInput[1].Votes);
+        }
+        
+        [Fact]
+        public void should_return_null_when_wrong_password()
+        {
+            var election = new Election();
+            var candidatesInput = GetMockCandidates(2);
+            election.TryCreateCandidates(candidatesInput, "Pa$$w0rd");
+            election.Vote(candidatesInput[0].Id);
+
+            var winners = election.Poll("WrongPassword123");
+
+            Assert.Null(winners[0]);
+        }
+
+        [Fact]
+        public void should_return_jonas_winner()
+        {
+            var election = new Election();
+            var candidatesInput = GetMockCandidates(2);
+            election.TryCreateCandidates(candidatesInput, "Pa$$w0rd");
+            election.Vote(candidatesInput[0].Id);
+
+            var winners = election.Poll("Pa$$w0rd");
+
+            Assert.Equal(candidatesInput[0].Name, winners[0].Name);
+        }
+
+        [Fact]
+        public void should_return_two_different_winners()
+        {
+            var election = new Election();
+            var candidatesInput = GetMockCandidates(2);
+            election.TryCreateCandidates(candidatesInput, "Pa$$w0rd");
+            election.Vote(candidatesInput[0].Id);
+            election.Vote(candidatesInput[1].Id);
+
+            var winners = election.Poll("Pa$$w0rd");
+
+            Assert.Equal(2, winners.Count);
+            Assert.True(candidatesInput[0].Id == winners[0].Id ^ candidatesInput[1].Id == winners[0].Id);
+            Assert.True(candidatesInput[0].Id == winners[1].Id ^ candidatesInput[1].Id == winners[1].Id);
+            Assert.NotEqual(winners[0].Id, winners[1].Id);
+        }
+    }
+}
